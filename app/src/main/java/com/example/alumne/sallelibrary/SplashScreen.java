835f39c9314a24771aad89ef0be74ff1ad901bb6
osaifh <raimon_to@hotmail.com>
2018-02-14 19:46:23 +0100

    second commit

---
+++ app/src/main/java/com/example/alumne/sallelibrary/SplashScreen.java
@@ -0,0 +1,22 @@
+package com.example.alumne.sallelibrary;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class SplashScreen extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash_screen);
+        try {
+            Thread.sleep(3000);
+        } catch (InterruptedException e) {
+
+        }
+        Intent intent = new Intent(this, MainActivity.class);
+        startActivity(intent);
+        finish();
+    }
+}
